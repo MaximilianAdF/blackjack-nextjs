bbf8dbf621ce7107f887a243fe1ca7817c2b37b0
Maximilian Alvim de Faria <maadf@kth.se>
2024-04-26 19:06:25 +0200

    Adds `Deck` class &  Updates package.json
    ## `Deck`:
    - Implementation of `Deck` class that stores the current `deckOfCards` that is being dealt
    - Method for creation of decks consisting of `numDecks` of seperate card decks, `createDeck()`.
    - Method for shuffling the deck, `shuffleDeck()`.
    - Method for dealing a card, returns card at top of stack or undefined if deck is empty, `dealCard()`.
    - Method that returns remaining number of cards in the deck, `getNumCards()`
    
    ## package.json
    - Adds `"type": "module"` to make it possible to use non-es import / export in .ts files

---
+++ app/Deck.ts
@@ -0,0 +1,54 @@
+import { Card } from './Card';
+
+export class Deck {
+    private deckOfCards: Card[]
+
+    constructor(numDecks: number = 1) {
+        this.deckOfCards = [];
+        this.createDeck(Math.min(numDecks, 8));
+    }
+
+    /**
+     * Create a deck of cards
+     * @param numDecks - number of 52-card decks used to create the deck
+     * @returns void
+     */
+    private createDeck(numDecks: number): void {
+        const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+        const suits = ['H', 'D', 'C', 'S'];
+        for (let i = 0; i < numDecks; i++) {
+            for (const suit of suits) {
+                for (const rank of ranks) {
+                    this.deckOfCards.push(new Card(rank+suit));
+                }
+            }
+        }
+    }
+
+    /**
+     * Shuffle the deck of cards
+     * @returns void
+     */
+    public shuffleDeck(): void {
+        for (let i = this.deckOfCards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.deckOfCards[i], this.deckOfCards[j]] = [this.deckOfCards[j], this.deckOfCards[i]];
+        }
+    }
+
+    /**
+     * Deal a card from the deck
+     * @returns Card | undefined
+     */
+    public dealCard(): Card | undefined {
+        return this.deckOfCards.pop();
+    }
+    
+    /**
+     * Get the number of cards in the deck
+     * @returns number
+     */
+    public getNumCards(): number {
+        return this.deckOfCards.length;
+    }
+}
