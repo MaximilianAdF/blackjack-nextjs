736509bc714c9dd23c6729d158f16527ec4dfc7b
Maximilian Alvim de Faria <maadf@kth.se>
2024-05-04 13:18:36 +0200

    Vercel Test

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
 
     return (
-
+      <div className="bg-gradient-radial from-blue-500 to-purple-500 h-screen flex justify-center items-center">
+      </div>
     );
 }
