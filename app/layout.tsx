8f5bccd4127a6fae5f0a1badc02729d6a0fc144e
Maximilian Alvim de Faria <maadf@kth.se>
2024-05-04 10:35:14 +0200

    Add Vercel deployment & analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -17,6 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
+      <SpeedInsights/>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,8 @@
 import Image from "next/image";
-import card from '../assets/cards/2C.svg';
-import chip from '../assets/chips/1000C.svg'
-
 
 export default function Home() {
-  return (
-    <><div className="flex items-center justify-center">
-      <Image
-        src={card}
-        alt="2 of Clubs"
-        width={100}
-        height={100} />
-    </div><div className="flex items-center justify-center">
-        <Image
-          src={chip}
-          alt="1000 Chip"
-          width={100}
-          height={100} />
-      </div></>
-  );
+
+    return (
+
+    );
 }
