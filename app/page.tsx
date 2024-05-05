b777c192e25bddcbfacfeb0d3b9e6a762ecda15c
Maximilian Alvim de Faria <maadf@kth.se>
2024-05-05 18:37:36 +0200

    Add dealer & card sections like reference image

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import card from "../assets/cards/2H.svg";
 
 
 /** TODO: Add grainy look to the suit symbols */
@@ -12,10 +13,10 @@ export default function Home() {
 
   const pattern = [];
   //Horizontal & vertical background pattern
-  for (let j = 0; j < 8; j++) {
-    for (let i = 0; i < 15; i++) {
+  for (let j = 0; j < 10; j++) {
+    for (let i = 0; i < 20; i++) {
       pattern.push(
-        <div key={"pattern row:"+j+" col:"+i} className="flex justify-center items-center" style={{ position: "relative", top: `${j * 100 + 70}px`, left: `${-100*(7-i)}px` }}>
+        <div key={"pattern row:"+j+" col:"+i} className="flex justify-center items-center" style={{ position: "relative", top: `${j * 100 + 70}px`, left: `${-100*(9-i)}px` }}>
           <div className="absolute m-auto mix-blend-screen opacity-15">
             <div className="dot"></div>
             <div className="line1"></div>
@@ -52,6 +53,10 @@ export default function Home() {
       <div className="pattern-container" style={{ zIndex: "-1" }}>
         {pattern}
       </div>
+      <div className="dealer-half-circle"></div>
+      <div className="half-ellipse-border-out"></div>
+      <div className="half-ellipse-border-in"></div>
+      <div className="hollow-half-ellipse"></div> 
     </div>
   );
 }
