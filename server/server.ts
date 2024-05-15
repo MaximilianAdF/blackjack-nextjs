ad7fb131d46072863db97b8e6f015f1ceaf3b113
Maximilian Alvim de Faria <maadf@kth.se>
2024-05-15 13:10:12 +0200

    a

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,9 @@
+import { Server } from 'socket.io';
+
 const express = require('express');
 const http = require('http');
 const app = express()
 const server = http.createServer(app);
-
-import { Server } from 'socket.io';
 
 const io = new Server(server, {
     cors: {
@@ -13,9 +13,10 @@ const io = new Server(server, {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+    // On a new connection, need to load the current game or create a new one if first player
+    // All the info/data for the game will be sent to the server and stored in memory (arrays, objects, etc)
+    // When new players join, they will be sent the current game state so it can be displayed on their screen
+    // When a player makes a move, the server will update the game state and send it to all players
 });
 
 server.listen(3001, () => {
